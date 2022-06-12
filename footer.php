d5b68028d8d1ac3f5f94d38e35e5a872a7b02620
CompaDesu <107257502+CompaDesu@users.noreply.github.com>
2022-06-12 18:31:01 +0100

    Cleaner footer and update version.
    ABXD 3.0 was never a stable release really anyway, though.

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 <br>
 <br>
 <?php $bucket = "footer"; include("./lib/pluginloader.php");?>
-Powered by AcmlmBoard XD 3.0<br>
-By Dirbaio, xfix, Kawa, StapleButter, Nadia, et al<br>
-AcmlmBoard &copy; Jean-Fran&ccedil;ois Lapointe<br>
+AcmlmBoard XD 3.0.1a1-mod<br />
+&copy; 2009-2019 The ABXD Team<br />
 
 <?=(isset($footerButtons) ? $footerButtons : "")?>
 <?=(isset($footerExtensionsB) ? $footerExtensionsB : "")?>
